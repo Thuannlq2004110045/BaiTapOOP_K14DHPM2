a725823cbb921b39a35479e455332e168bf5e757
Thuannlq2004110045 <quangthuan672@gmail.com>
2022-02-21 11:01:54 +0700

    nộp bài đoán số

---
+++ src/k14dhpm2/tuan2/buoi2/GameLauncher.java
@@ -0,0 +1,8 @@
+package k14dhpm2.tuan2.buoi2;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ src/k14dhpm2/tuan2/buoi2/Player.java
@@ -0,0 +1,10 @@
+package k14dhpm2.tuan2.buoi2;
+
+public class Player {
+    int number = 0;
+    public void guess()
+    {
+        number = (int)(Math.random()*10);
+        System.out.println("I'm guessing " + number);
+    }
+}
